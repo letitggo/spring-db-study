8d1c27ad17591c4811390258c3e19a581a058d8f
Seong57 <gustjr7297@naver.com>
2025-03-11 23:28:42 +0900

    ddl: 락 테스트
    콘솔을 두개 열어서 서로 다른 트랜잭션으로 테스트
    memberId에는 인덱스가 있기 때문에 잠기는 row가 많지 않지만
    where createdAt(인덱스 없음)으로 락을 걸면 모든 row가 락에 걸려버린다.

--- st_sns/src/main/java/com/example/demo/shcema/ddl.sql
+++ st_sns/src/main/java/com/example/demo/shcema/ddl.sql
@@ -67,4 +67,17 @@ create table Timeline
     primary key (id)
 );
 
+/*
+mysql에서 락은 row가 아니라 인덱스를 잠근다
+따라서 인덱스가 없는 조건으로 Locking read시 불필요한 데이터들이 잠길 수 있다.
+*/
+start transaction;
+select *
+from POST
+where memberId = 3 and createdAt = '2025-03-10 22:09:09' for update;
+commit;
+
+select count(*) from performance_schema.data_locks;
+select * from information_schema.innodb_trx;
+
 
