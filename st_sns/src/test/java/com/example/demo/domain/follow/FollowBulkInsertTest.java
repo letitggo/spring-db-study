c0856b7989f7e22225f995f8c042f95e66f0342c
Seong57 <gustjr7297@naver.com>
2025-03-12 18:47:14 +0900

    test: 팔로워, 팔로잉이 100명인 회원

--- st_sns/src/test/java/com/example/demo/domain/follow/FollowBulkInsertTest.java
+++ st_sns/src/test/java/com/example/demo/domain/follow/FollowBulkInsertTest.java
@@ -4,6 +4,7 @@ import com.example.demo.domain.follow.entity.Follow;
 import com.example.demo.domain.follow.repository.FollowRepository;
 import com.example.demo.util.FollowFixtureFactory;
 import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,16 +13,32 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootTest
-public class FollowTest {
+public class FollowBulkInsertTest {
 
     @Autowired
     private FollowRepository followRepository;
 
+    @DisplayName("팔로워 만들기(id=1 회원의 팔로워를 100만명 만들기)")
     @Test
-    void bulkInsert() {
+    void bulkInsertByToMemberId() {
         // id 1 회원의 팔로워를 100만으로 만듦
-        EasyRandom easyRandom = FollowFixtureFactory.get(
+        EasyRandom easyRandom = FollowFixtureFactory.getByToMemberId(
                 1L
+        );
+
+        List<Follow> follows = IntStream.range(0, 1_000_000)
+                .mapToObj(i -> easyRandom.nextObject(Follow.class))
+                .toList();
+
+        followRepository.bulkInsert(follows);
+    }
+
+    @DisplayName("팔로우 하기(id=2 회원의 팔로잉을 100만명으로 만들기)")
+    @Test
+    void bulkInsertByFromMemberId() {
+        // id 1 회원의 팔로워를 100만으로 만듦
+        EasyRandom easyRandom = FollowFixtureFactory.getByFromMemberId(
+                2L
         );
 
         List<Follow> follows = IntStream.range(0, 1_000_000)
--- st_sns/src/test/java/com/example/demo/util/FollowFixtureFactory.java
+++ st_sns/src/test/java/com/example/demo/util/FollowFixtureFactory.java
@@ -11,7 +11,8 @@ import static org.jeasy.random.FieldPredicates.*;
 
 public class FollowFixtureFactory {
 
-    public static EasyRandom get(Long toMemberId) {
+    // 팔로워 만들기
+    public static EasyRandom getByToMemberId(Long toMemberId) {
         Predicate<Field> idPredicated = named("id")
                 .and(ofType(Long.class))
                 .and(inClass(Follow.class));
@@ -29,6 +30,29 @@ public class FollowFixtureFactory {
                 .randomize(toMemberIdPredicated, () -> toMemberId)
                 // long, int 범위를 지정할 수 없나...?
                 .randomize(fromMemberIdPredicated, () -> (long)((Math.random() * 1000000) + 1));
+
+        return new EasyRandom(params);
+    }
+
+    // 팔로우하기
+    public static EasyRandom getByFromMemberId(Long fromMemberId) {
+        Predicate<Field> idPredicated = named("id")
+                .and(ofType(Long.class))
+                .and(inClass(Follow.class));
+
+        Predicate<Field> fromMemberIdPredicated = named("fromMemberId")
+                .and(ofType(Long.class))
+                .and(inClass(Follow.class));
+
+        Predicate<Field> toMemberIdPredicated = named("toMemberId")
+                .and(ofType(Long.class))
+                .and(inClass(Follow.class));
+
+        EasyRandomParameters params = new EasyRandomParameters()
+                .excludeField(idPredicated)
+                .randomize(fromMemberIdPredicated, () -> fromMemberId)
+                // long, int 범위를 지정할 수 없나...?
+                .randomize(toMemberIdPredicated, () -> (long)((Math.random() * 1000000) + 1));
 
         return new EasyRandom(params);
     }
