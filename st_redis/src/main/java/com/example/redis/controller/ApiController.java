39390f72569633d9fb4dc92bab551e00f3bb09dd
Seong57 <gustjr7297@naver.com>
2025-03-16 21:25:59 +0900

    feat: cache aside

---
+++ st_redis/src/main/java/com/example/redis/controller/ApiController.java
@@ -0,0 +1,20 @@
+package com.example.redis.controller;
+
+import com.example.redis.dto.UserProfile;
+import com.example.redis.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ApiController {
+
+    private final UserService userService;
+
+    @GetMapping("/users/{userId}/profile")
+    public UserProfile getUserProfile(@PathVariable String userId) {
+        return userService.getUserProfile(userId);
+    }
+}
